04d75348c46f93c2a72e64d8a6b4db36808cf3e6
szcpitt <szcpitt@users.noreply.github.com>
2018-11-21 16:07:04 -0500

    add word match

--- src/main/java/com/recipesearch/recipeSearch/Service/RecipeService.java
+++ src/main/java/com/recipesearch/recipeSearch/Service/RecipeService.java
@@ -5,6 +5,8 @@ import com.recipesearch.recipeSearch.Repository.RecipeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class RecipeService {
 
@@ -13,5 +15,9 @@ public class RecipeService {
 
     public Recipe getOne(String name){
         return recipeRepo.findByName(name);
+    }
+
+    public List<Recipe> getRecipeList(String name){
+        return recipeRepo.findByNameContaining(name);
     }
 }
